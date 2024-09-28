282d35407b92a5bfa65091c980f381c9221075c2
Sangho <sh31659@gmail.com>
2024-09-27 20:10:10 -0400

    Add visitor tracking code

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,6 +5,9 @@ const Footer: React.FC = () => {
     return (
         <footer className="mt-8 py-4 text-center text-gray-600">
             <p>© UIST 2024 Dynamic Abstraction Workshop</p>
+            <div style="display: none;">
+                <script type="text/javascript" id="clustrmaps" src="//clustrmaps.com/map_v2.js?d=tWG8RqUFpZ6XnwRDFuvGwsMaB_7HqCbJxJGHQZMF6eE&cl=ffffff&w=a"></script>
+            </div>
         </footer>
     );
 };
