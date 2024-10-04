5d4aa3af9f266148fe5c13fa8c781f605a72c154
ryan-waterloo <r4yen@uwaterloo.ca>
2024-10-04 11:01:27 -0400

    remove unused visitor tracking code

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,10 +5,10 @@ const Footer: React.FC = () => {
     return (
         <footer className="mt-8 py-4 text-center text-gray-600">
             <p>© UIST 2024 Dynamic Abstraction Workshop</p>
-            <div style="display: none;">
+            {/* <div style="display: none;">
                 <script type="text/javascript" id="clustrmaps" src="//clustrmaps.com/map_v2.js?d=tWG8RqUFpZ6XnwRDFuvGwsMaB_7HqCbJxJGHQZMF6eE&cl=ffffff&w=a"></script>
                 <script type="text/javascript" id="clustrmaps" src="//clustrmaps.com/map_v2.js?d=r7k3lLw_SqZirZdVuYiq1UYuknvpBU-8dp7gPLeTcJA&cl=ffffff&w=a"></script>
-            </div>
+            </div> */}
         </footer>
     );
 };
